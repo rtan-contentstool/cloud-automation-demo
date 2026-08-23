7701398e7138e0307c4f7408c6d599f5f3ec70b2
rtan-contentstool <contentstool@gmail.com>
2026-08-23 19:50:42 +0900

    Add Docker build workflow

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/plain; charset=utf-8"
+    });
+
+    res.end("Cloud Automation App v1");
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
